31296c72d27b1f74f0bb42ea44046e50b19e302d
eapenm <eapenmani@gmail.com>
2023-05-01 11:46:05 +0000

    Created scripts to build and push the docker image to ecr

--- bin/ecr/ecr-create.py
+++ bin/ecr/ecr-create.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
+
 import boto3
 
 ecr_client = boto3.client('ecr')
-repository_name = "system-monitoring-app-repo"
+repository_name = 'system-monitoring-app-repo'
 response = ecr_client.create_repository(repositoryName=repository_name)
 
 repository_uri = response['repository']['repositoryUri']
 print(repository_uri)
+
